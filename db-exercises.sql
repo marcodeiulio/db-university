d10a333046f0b766ad9b117b2c5add30af2a2488
marcodeiulio <mdeiulio@icloud.com>
2022-03-09 17:09:15 +0100

    - Added ex 5

--- db-exercises.sql
+++ db-exercises.sql
@@ -20,3 +20,9 @@ SELECT *
 FROM `courses`
 WHERE `period` = 'I semestre'
 AND `year` = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT *
+FROM `exams`
+WHERE date(`date`) = '2020-06-20'
+AND hour(`hour`) >= 14;
