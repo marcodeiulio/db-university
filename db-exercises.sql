959d595ccd0a3b2c17dc22f6799e7c0432bf659a
marcodeiulio <mdeiulio@icloud.com>
2022-03-09 16:47:14 +0100

    - Added ex 2 & 3

--- db-exercises.sql
+++ db-exercises.sql
@@ -2,3 +2,15 @@
 SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) = '1990';
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT *
+FROM `courses`
+WHERE `cfu` > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM `students`
+WHERE YEAR(`date_of_birth`) < 1992
+ORDER BY `date_of_birth`;
+
