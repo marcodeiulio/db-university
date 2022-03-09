ce6f81d0fe149a4628fe38a213457c044b1f2e9c
marcodeiulio <mdeiulio@icloud.com>
2022-03-09 17:59:40 +0100

    - Fixed ex 1

--- db-exercises.sql
+++ db-exercises.sql
@@ -1,7 +1,7 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990
 SELECT *
 FROM `students`
-WHERE YEAR(`date_of_birth`) = '1990';
+WHERE YEAR(`date_of_birth`) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti
 SELECT *
